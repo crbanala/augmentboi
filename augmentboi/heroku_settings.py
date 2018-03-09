fc8ef8e9d902fff2e9370b350fa2dc298d724beb
maniteja <teja3536mani@gmail.com>
2018-03-09 19:14:02 +0530

    Static

--- augmentboi/heroku_settings.py
+++ augmentboi/heroku_settings.py
@@ -4,3 +4,6 @@ import dj_database_url
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
